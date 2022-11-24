9ef791019c4367aa68517a907e8e9113153c022e
Michael <michael.kroupa@mail.schwarz>
2022-11-24 08:46:23 +0100

    Hide Android Redirect Error

--- lib/src/presentation/auth_web_view.dart
+++ lib/src/presentation/auth_web_view.dart
@@ -1,12 +1,10 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:logging/logging.dart';
 
 import '../domain/export.dart';
 
-
-class AuthWebView extends StatelessWidget {
+class AuthWebView extends StatefulWidget {
   static final _log = Logger('$AuthWebView');
 
   final ViewType type;
@@ -22,11 +20,18 @@ class AuthWebView extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<AuthWebView> createState() => _AuthWebViewState();
+}
+
+class _AuthWebViewState extends State<AuthWebView> {
+  bool _hideRedirect = false;
+
+  @override
   Widget build(BuildContext context) {
     return FutureBuilder<Uri>(
-      future: repository.generateAuthRequestUrl(type),
+      future: widget.repository.generateAuthRequestUrl(widget.type),
       builder: (context, snapshot) {
-        if (!snapshot.hasData) {
+        if (!snapshot.hasData || _hideRedirect) {
           return const Center(child: CircularProgressIndicator());
         }
         return InAppWebView(
@@ -51,20 +56,31 @@ class AuthWebView extends StatelessWidget {
   Future<void> _loadError(InAppWebViewController controller, Uri? url, int code,
       String message) async {
     if (url == null) return;
-    if (_isRedirectUrl(url)) {
-      _log.info('✅ Recognized redirect after login success');
-      final code = url.queryParameters['code'];
-      final state = url.queryParameters['state'];
-      await repository.finishLogin(code ?? '', state ?? '');
-      onSuccess();
-      return;
+    try {
+      if (_isRedirectUrl(url)) {
+        setState(() {
+          _hideRedirect = true;
+        });
+        AuthWebView._log.info('✅ Recognized redirect after login success');
+        final code = url.queryParameters['code'];
+        final state = url.queryParameters['state'];
+        await widget.repository.finishLogin(code ?? '', state ?? '');
+        widget.onSuccess();
+        return;
+      }
+      AuthWebView._log.info('💥 Load error: $code, $url, $message');
+    } finally {
+      setState(() {
+        _hideRedirect = false;
+      });
     }
-    _log.info('💥 Load error: $code, $url, $message');
   }
 
   bool _isRedirectUrl(Uri url) {
-    if (url.scheme != repository.config.redirectUri.split(':')[0]) return false;
-    if (url.host != repository.config.redirectUri.split('/').last) return false;
+    if (url.scheme != widget.repository.config.redirectUri.split(':')[0])
+      return false;
+    if (url.host != widget.repository.config.redirectUri.split('/').last)
+      return false;
     if (url.queryParameters['code'] == null) return false;
     if (url.queryParameters['state'] == null) return false;
 
